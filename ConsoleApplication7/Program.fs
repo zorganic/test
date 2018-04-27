d709aeb8d6cd6c542edb0cbf00e13c5921e63a2e
zorganic <albert.yacobov@gmail.com>
2018-04-27 20:12:49 +0300

    qwewq

---
+++ ConsoleApplication7/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open System.Threading
+open Suave
+
+[<EntryPoint>]
+let main argv = 
+  let cts = new CancellationTokenSource()
+  let conf = { defaultConfig with cancellationToken = cts.Token }
+  let listening, server = startWebServerAsync conf (Successful.OK "Hello World")
+    
+  Async.Start(server, cts.Token)
+  printfn "Make requests now"
+  Console.ReadKey true |> ignore
+    
+  cts.Cancel()
+
+  0 // return an integer exit code
